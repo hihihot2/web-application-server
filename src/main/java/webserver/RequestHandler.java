e728d1b34f5fd2b7cc555528485fda38eacb750e
hihihot2 <17679@hanmail.net>
2017-02-14 13:17:16 +0900

    ㅁㄴㅇㄹ

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -13,6 +13,8 @@ import java.nio.file.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import model.User;
+
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
@@ -48,7 +50,7 @@ public class RequestHandler extends Thread {
         	
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp"+headerInfo[1]).toPath());
+            byte[] body = Files.readAllBytes(new File("./webapp"+returnUrl(headerInfo[1])).toPath());
             response200Header(dos, body.length, contentType);
             responseBody(dos, body);
 
@@ -76,5 +78,41 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+    
+    private String returnUrl(String inputInfo) {
+    	log.debug("Url : "+inputInfo);
+    	if(inputInfo.equals("/")) {
+    		return "/index.html";
+    	}
+    	
+    	if(inputInfo.contains("?")) {
+    		String[] tokenUrl = inputInfo.split("?");
+    		
+    		switch(tokenUrl[0]) {
+    		case "/user/create":
+    			User user = null;
+    			String[] tokenData = tokenUrl[1].split("&");
+    			matchingData(tokenUrl[0], tokenData);
+    			break;
+    		}
+    		return tokenUrl[0];
+    	}else {
+    		return inputInfo;
+    	}
+    }
+    
+    private boolean matchingData(String url, String[] tokenData) {
+    	switch(url) {
+    	case "":
+    		break;
+    	}
+    	
+    	for(String data : tokenData) {
+    		if(data.contains("&")){
+    			
+    		}
+    	}
+    	return false;
     }
 }
